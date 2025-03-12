b0ed898da563abe0dd67eea131e378caa8508b8b
mahnoor3650 <mahnoorkhalidhashmi138@gmail.com>
2025-03-12 22:49:57 +0500

    navbar and hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Navbar from './component/Navbar'
+import Hero from './component/Hero'
+
 
 const App = () => {
   return (
-    <div className='text-red-800'>
-      hello world
+   <main className='mx-auto max-w-7xl overflow-x-hidden antialiased'>
+    <div className='bg-image fixed inset-0 bg-cover bg-fixed bg-center'>
+      <div className='relative z-10'>
+        <Navbar />
+       <Hero />
+      </div>
     </div>
+   </main>
   )
 }
 
